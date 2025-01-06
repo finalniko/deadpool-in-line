c55b82469382aa672627247f20494a4dd54c202e
finalniko <nikoloztkemaladze0910@gmail.com>
2025-01-06 15:42:52 +0400

    deadpool

---
+++ deadpool.cpp
@@ -0,0 +1,450 @@
+#include <windows.h>
+
+int main()
+{
+	HDC hdc = GetDC(0);
+	HPEN hp = CreatePen(0, 10, RGB(255, 255, 255));
+	SelectObject(hdc, hp);
+
+	while (1)
+	{
+		MoveToEx(hdc, 625, 1075, 0);
+		LineTo(hdc, 500, 1075);
+
+		MoveToEx(hdc, 500, 1075, 0);
+		LineTo(hdc, 500, 950);
+
+		MoveToEx(hdc, 500, 950, 0);
+		LineTo(hdc, 525, 950);
+
+		MoveToEx(hdc, 525, 950, 0);
+		LineTo(hdc, 525, 875);
+
+		MoveToEx(hdc, 525, 875, 0);
+		LineTo(hdc, 550, 875);
+
+		MoveToEx(hdc, 550, 875, 0);
+		LineTo(hdc, 550, 800);
+
+		MoveToEx(hdc, 550, 800, 0);
+		LineTo(hdc, 500, 800);
+
+		MoveToEx(hdc, 500, 800, 0);
+		LineTo(hdc, 500, 775);
+
+		MoveToEx(hdc, 500, 775, 0);
+		LineTo(hdc, 475, 775);
+
+		MoveToEx(hdc, 475, 775, 0);
+		LineTo(hdc, 450, 775);
+
+		MoveToEx(hdc, 450, 775, 0);
+		LineTo(hdc, 450, 650);
+
+		MoveToEx(hdc, 450, 650, 0);
+		LineTo(hdc, 475, 650);
+
+		MoveToEx(hdc, 475, 650, 0);
+		LineTo(hdc, 475, 575);
+
+		MoveToEx(hdc, 475, 575, 0);
+		LineTo(hdc, 500, 575);
+
+		MoveToEx(hdc, 500, 575, 0);
+		LineTo(hdc, 500, 525);
+
+		MoveToEx(hdc, 500, 525, 0);
+		LineTo(hdc, 525, 525);
+
+		MoveToEx(hdc, 525, 525, 0);
+		LineTo(hdc, 525, 475);
+
+		MoveToEx(hdc, 525, 475, 0);
+		LineTo(hdc, 500, 475);
+
+		MoveToEx(hdc, 500, 475, 0);
+		LineTo(hdc, 500, 450);
+
+		MoveToEx(hdc, 500, 450, 0);
+		LineTo(hdc, 475, 450);
+
+		MoveToEx(hdc, 500, 450, 0);
+		LineTo(hdc, 475, 450);
+
+		MoveToEx(hdc, 475, 450, 0);
+		LineTo(hdc, 475, 425);
+
+		MoveToEx(hdc, 475, 425, 0);
+		LineTo(hdc, 450, 425);
+
+		MoveToEx(hdc, 450, 425, 0);
+		LineTo(hdc, 450, 400);
+
+		MoveToEx(hdc, 450, 400, 0);
+		LineTo(hdc, 425, 400);
+
+		MoveToEx(hdc, 425, 400, 0);
+		LineTo(hdc, 425, 375);
+
+		MoveToEx(hdc, 425, 375, 0);
+		LineTo(hdc, 400, 375);
+
+		MoveToEx(hdc, 400, 375, 0);
+		LineTo(hdc, 400, 325);
+
+		MoveToEx(hdc, 400, 325, 0);
+		LineTo(hdc, 450, 325);
+
+		MoveToEx(hdc, 450, 325, 0);
+		LineTo(hdc, 450, 350);
+
+		MoveToEx(hdc, 450, 350, 0);
+		LineTo(hdc, 475, 350);
+
+		MoveToEx(hdc, 475, 350, 0);
+		LineTo(hdc, 475, 375);
+
+		MoveToEx(hdc, 475, 375, 0);
+		LineTo(hdc, 500, 375);
+
+		MoveToEx(hdc, 500, 375, 0);
+		LineTo(hdc, 500, 400);
+
+		MoveToEx(hdc, 500, 400, 0);
+		LineTo(hdc, 525, 400);
+
+		MoveToEx(hdc, 525, 400, 0);
+		LineTo(hdc, 525, 425);
+
+		MoveToEx(hdc, 525, 425, 0);
+		LineTo(hdc, 550, 425);
+
+		MoveToEx(hdc, 550, 425, 0);
+		LineTo(hdc, 550, 450);
+
+		MoveToEx(hdc, 550, 450, 0);
+		LineTo(hdc, 575, 450);
+
+		MoveToEx(hdc, 575, 450, 0);
+		LineTo(hdc, 575, 425);
+
+		MoveToEx(hdc, 575, 425, 0);
+		LineTo(hdc, 600, 425);
+
+		MoveToEx(hdc, 600, 425, 0);
+		LineTo(hdc, 600, 400);
+
+		MoveToEx(hdc, 600, 425, 0);
+		LineTo(hdc, 625, 425);
+
+		MoveToEx(hdc, 625, 425, 0);
+		LineTo(hdc, 625, 450);
+
+		MoveToEx(hdc, 625, 425, 0);
+		LineTo(hdc, 625, 450);
+
+		MoveToEx(hdc, 625, 450, 0);
+		LineTo(hdc, 675, 450);
+
+		MoveToEx(hdc, 675, 450, 0);
+		LineTo(hdc, 675, 475);
+
+		MoveToEx(hdc, 675, 475, 0);
+		LineTo(hdc, 800, 475);
+
+		MoveToEx(hdc, 800, 475, 0);
+		LineTo(hdc, 800, 450);
+
+		MoveToEx(hdc, 800, 450, 0);
+		LineTo(hdc, 825, 450);
+
+		MoveToEx(hdc, 600, 400, 0);
+		LineTo(hdc, 575, 400);
+
+		MoveToEx(hdc, 575, 400, 0);
+		LineTo(hdc, 575, 375);
+
+		MoveToEx(hdc, 575, 375, 0);
+		LineTo(hdc, 550, 375);
+
+		MoveToEx(hdc, 550, 375, 0);
+		LineTo(hdc, 550, 325);
+
+		MoveToEx(hdc, 550, 325, 0);
+		LineTo(hdc, 525, 325);
+
+		MoveToEx(hdc, 525, 325, 0);
+		LineTo(hdc, 525, 200);
+
+		MoveToEx(hdc, 525, 200, 0);
+		LineTo(hdc, 550, 200);
+
+		MoveToEx(hdc, 550, 200, 0);
+		LineTo(hdc, 550, 150);
+
+		MoveToEx(hdc, 550, 150, 0);
+		LineTo(hdc, 575, 150);
+
+		MoveToEx(hdc, 575, 150, 0);
+		LineTo(hdc, 575, 125);
+
+		MoveToEx(hdc, 575, 125, 0);
+		LineTo(hdc, 600, 125);
+
+		MoveToEx(hdc, 600, 125, 0);
+		LineTo(hdc, 600, 100);
+
+		MoveToEx(hdc, 600, 100, 0);
+		LineTo(hdc, 650, 100);
+
+		MoveToEx(hdc, 650, 100, 0);
+		LineTo(hdc, 650, 75);
+
+		MoveToEx(hdc, 650, 75, 0);
+		LineTo(hdc, 775, 75);
+
+		MoveToEx(hdc, 775, 75, 0);
+		LineTo(hdc, 775, 100);
+
+		MoveToEx(hdc, 775, 100, 0);
+		LineTo(hdc, 825, 100);
+
+		MoveToEx(hdc, 825, 100, 0);
+		LineTo(hdc, 825, 125);
+
+		MoveToEx(hdc, 825, 125, 0);
+		LineTo(hdc, 850, 125);
+
+		MoveToEx(hdc, 850, 125, 0);
+		LineTo(hdc, 850, 150);
+
+		MoveToEx(hdc, 850, 150, 0);
+		LineTo(hdc, 875, 150);
+
+		MoveToEx(hdc, 875, 150, 0);
+		LineTo(hdc, 875, 200);
+
+		MoveToEx(hdc, 875, 200, 0);
+		LineTo(hdc, 900, 200);
+
+		MoveToEx(hdc, 900, 200, 0);
+		LineTo(hdc, 900, 325);
+
+		MoveToEx(hdc, 900, 325, 0);
+		LineTo(hdc, 875, 325);
+
+		MoveToEx(hdc, 875, 325, 0);
+		LineTo(hdc, 875, 400);
+
+		MoveToEx(hdc, 875, 400, 0);
+		LineTo(hdc, 850, 400);
+
+		MoveToEx(hdc, 850, 400, 0);
+		LineTo(hdc, 850, 425);
+
+		MoveToEx(hdc, 850, 425, 0);
+		LineTo(hdc, 825, 425);
+
+		MoveToEx(hdc, 825, 425, 0);
+		LineTo(hdc, 825, 450);
+
+		MoveToEx(hdc, 825, 450, 0);
+		LineTo(hdc, 850, 450);
+
+		MoveToEx(hdc, 850, 450, 0);
+		LineTo(hdc, 850, 475);
+
+		MoveToEx(hdc, 850, 450, 0);
+		LineTo(hdc, 850, 475);
+
+		MoveToEx(hdc, 850, 475, 0);
+		LineTo(hdc, 875, 475);
+
+		MoveToEx(hdc, 875, 475, 0);
+		LineTo(hdc, 875, 550);
+
+		MoveToEx(hdc, 875, 550, 0);
+		LineTo(hdc, 900, 550);
+
+		MoveToEx(hdc, 900, 550, 0);
+		LineTo(hdc, 900, 625);
+
+		MoveToEx(hdc, 900, 625, 0);
+		LineTo(hdc, 925, 625);
+
+		MoveToEx(hdc, 925, 625, 0);
+		LineTo(hdc, 925, 725);
+
+		MoveToEx(hdc, 925, 725, 0);
+		LineTo(hdc, 950, 725);
+
+		MoveToEx(hdc, 950, 725, 0);
+		LineTo(hdc, 950, 825);
+
+		MoveToEx(hdc, 950, 825, 0);
+		LineTo(hdc, 875, 825);
+
+		MoveToEx(hdc, 875, 825, 0);
+		LineTo(hdc, 875, 975);
+
+		MoveToEx(hdc, 875, 975, 0);
+		LineTo(hdc, 900, 975);
+
+		MoveToEx(hdc, 900, 975, 0);
+		LineTo(hdc, 900, 1000);
+
+		MoveToEx(hdc, 900, 1000, 0);
+		LineTo(hdc, 925, 1000);
+
+		MoveToEx(hdc, 925, 1000, 0);
+		LineTo(hdc, 925, 1050);
+
+		MoveToEx(hdc, 925, 1050, 0);
+		LineTo(hdc, 775, 1050);
+
+		MoveToEx(hdc, 775, 1050, 0);
+		LineTo(hdc, 775, 1025);
+
+		MoveToEx(hdc, 775, 1025, 0);
+		LineTo(hdc, 750, 1025);
+
+		MoveToEx(hdc, 750, 1025, 0);
+		LineTo(hdc, 750, 1000);
+
+		MoveToEx(hdc, 750, 1000, 0);
+		LineTo(hdc, 725, 1000);
+
+		MoveToEx(hdc, 725, 1000, 0);
+		LineTo(hdc, 725, 750);
+
+		MoveToEx(hdc, 725, 875, 0);
+		LineTo(hdc, 700, 875);
+
+		MoveToEx(hdc, 700, 875, 0);
+		LineTo(hdc, 700, 900);
+
+		MoveToEx(hdc, 700, 900, 0);
+		LineTo(hdc, 675, 900);
+
+		MoveToEx(hdc, 675, 900, 0);
+		LineTo(hdc, 675, 950);
+
+		MoveToEx(hdc, 675, 950, 0);
+		LineTo(hdc, 650, 950);
+
+		MoveToEx(hdc, 650, 950, 0);
+		LineTo(hdc, 650, 1000);
+
+		MoveToEx(hdc, 650, 1000, 0);
+		LineTo(hdc, 625, 1000);
+
+		MoveToEx(hdc, 625, 1000, 0);
+		LineTo(hdc, 625, 1075);
+
+
+		MoveToEx(hdc, 625, 750, 0);
+		LineTo(hdc, 600, 750);
+
+		MoveToEx(hdc, 625, 750, 0);
+		LineTo(hdc, 625, 700);
+
+		MoveToEx(hdc, 625, 700, 0);
+		LineTo(hdc, 650, 700);
+
+		MoveToEx(hdc, 650, 700, 0);
+		LineTo(hdc, 650, 625);
+
+		MoveToEx(hdc, 650, 625, 0);
+		LineTo(hdc, 600, 625);
+
+		MoveToEx(hdc, 600, 625, 0);
+		LineTo(hdc, 600, 650);
+
+		MoveToEx(hdc, 600, 650, 0);
+		LineTo(hdc, 575, 650);
+
+		MoveToEx(hdc, 575, 650, 0);
+		LineTo(hdc, 575, 725);
+
+		MoveToEx(hdc, 575, 725, 0);
+		LineTo(hdc, 600, 725);
+
+		MoveToEx(hdc, 600, 725, 0);
+		LineTo(hdc, 600, 750);
+
+		MoveToEx(hdc, 675, 375, 0);
+		LineTo(hdc, 750, 375);
+
+		MoveToEx(hdc, 750, 375, 0);
+		LineTo(hdc, 750, 350);
+
+		MoveToEx(hdc, 750, 350, 0);
+		LineTo(hdc, 775, 350);
+
+		MoveToEx(hdc, 775, 350, 0);
+		LineTo(hdc, 775, 250);
+
+		MoveToEx(hdc, 775, 250, 0);
+		LineTo(hdc, 725, 250);
+
+		MoveToEx(hdc, 725, 250, 0);
+		LineTo(hdc, 725, 225);
+
+		MoveToEx(hdc, 725, 225, 0);
+		LineTo(hdc, 650, 225);
+
+		MoveToEx(hdc, 650, 225, 0);
+		LineTo(hdc, 650, 250);
+
+		MoveToEx(hdc, 650, 250, 0);
+		LineTo(hdc, 625, 250);
+
+		MoveToEx(hdc, 625, 250, 0);
+		LineTo(hdc, 625, 325);
+
+		MoveToEx(hdc, 625, 325, 0);
+		LineTo(hdc, 650, 325);
+
+		MoveToEx(hdc, 650, 325, 0);
+		LineTo(hdc, 650, 350);
+
+		MoveToEx(hdc, 650, 350, 0);
+		LineTo(hdc, 675, 350);
+
+		MoveToEx(hdc, 675, 350, 0);
+		LineTo(hdc, 675, 375);
+
+		MoveToEx(hdc, 675, 325, 0);
+		LineTo(hdc, 750, 325);
+
+		MoveToEx(hdc, 750, 325, 0);
+		LineTo(hdc, 750, 300);
+
+		MoveToEx(hdc, 750, 300, 0);
+		LineTo(hdc, 725, 300);
+
+		MoveToEx(hdc, 725, 300, 0);
+		LineTo(hdc, 725, 275);
+
+		MoveToEx(hdc, 725, 275, 0);
+		LineTo(hdc, 675, 275);
+
+		MoveToEx(hdc, 675, 275, 0);
+		LineTo(hdc, 675, 325);
+
+		MoveToEx(hdc, 875, 350, 0);
+		LineTo(hdc, 850, 350);
+
+		MoveToEx(hdc, 850, 350, 0);
+		LineTo(hdc, 850, 250);
+
+		MoveToEx(hdc, 850, 250, 0);
+		LineTo(hdc, 875, 250);
+
+		MoveToEx(hdc, 875, 250, 0);
+		LineTo(hdc, 875, 225);
+
+		MoveToEx(hdc, 875, 225, 0);
+		LineTo(hdc, 900, 225);
+	}
+}
